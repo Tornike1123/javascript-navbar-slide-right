8540e63be74301eef0d02a7d68ddadc448a3720f
Tornike1123 <toko36106@gmail.com>
2022-01-14 02:53:46 +0400

    change

---
+++ mine.js
@@ -0,0 +1,26 @@
+let button = document.querySelector('#butt');
+let butt2 = document.querySelector('#slide2');
+
+button.addEventListener("click", function(e){
+    e.preventDefault();
+  
+    butt2.classList.toggle('show')
+
+  })
+  
+
+
+  let buts = document.querySelector('#butt');
+  let buts2 = document.querySelector(".fa-bars");
+
+  buts.onclick = function(){
+    if(buts2.classList.contains("fa-bars")){
+      buts2.classList.replace("fa-bars","fa-times");
+    }else{
+      buts2.classList.replace("fa-times","fa-bars");
+    }
+  }
+
+
+
+  
