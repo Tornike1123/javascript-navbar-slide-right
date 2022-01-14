4c7fe4f3705f95b48896678054257450a09ae745
Tornike1123 <toko36106@gmail.com>
2022-01-15 00:12:20 +0400

    change

--- mine.js
+++ mine.js
@@ -11,13 +11,13 @@ button.addEventListener("click", function(e){
 
 
   let buts = document.querySelector('#butt');
-  let buts2 = document.querySelector(".fa-bars");
+  let buts2 = document.querySelector(".fa-times");
 
   buts.onclick = function(){
-    if(buts2.classList.contains("fa-bars")){
-      buts2.classList.replace("fa-bars","fa-times");
-    }else{
+    if(buts2.classList.contains("fa-times")){
       buts2.classList.replace("fa-times","fa-bars");
+    }else{
+      buts2.classList.replace("fa-bars","fa-times");
     }
   }
 
